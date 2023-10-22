53463aa3255421bcf3bbeaec7fc214317be56fef
ShankarSubramanyam055 <134670042+ShankarSubramanyam055@users.noreply.github.com>
2023-10-22 07:40:56 +0530

    Add files via upload

---
+++ js/owl-carousel.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+$(function() {
+  // Owl Carousel
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 3,
+    margin: 10,
+    loop: true,
+    nav: true
+  });
+});
